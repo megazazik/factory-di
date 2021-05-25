c7a9b1f6ead2003f494aa3e41e1bda85e29fdb97
Lebedev Mikhail <lebedev.mv@tutu.ru>
2021-05-26 01:59:01 +0300

    feat(types): add AllDependenciesOfContainer type helper

--- src/containerData.ts
+++ src/containerData.ts
@@ -17,17 +17,14 @@ export type GetValue<Type, Deps extends Dependencies> = (
 	resolve: <K extends keyof Deps>(k: K) => Deps[K]
 ) => Type;
 
-export type RegisteredDepsOfContainer<
-	T extends ContainerData<any, any, any>
-> = T['registeredDeps'];
+export type RegisteredDepsOfContainer<T extends ContainerData<any, any, any>> =
+	T['registeredDeps'];
 
-export type DepsOfContainer<
-	T extends ContainerData<any, any, any>
-> = T[typeof DepsSymbol];
+export type DepsOfContainer<T extends ContainerData<any, any, any>> =
+	T[typeof DepsSymbol];
 
-export type ValueOfContainer<
-	T extends ContainerData<any, any, any>
-> = ReturnType<T['getValue']>;
+export type ValueOfContainer<T extends ContainerData<any, any, any>> =
+	ReturnType<T['getValue']>;
 
 export type FlatDependenciesUnion<
 	Deps extends Record<Key, ContainerData<any, any, any>>
@@ -41,6 +38,9 @@ export type FlatDependenciesUnion<
 export type FlatDependencies<
 	Deps extends Record<Key, ContainerData<any, any, any>>
 > = UnionToIntersection<FlatDependenciesUnion<Deps>>;
+
+export type AllDependenciesOfContainer<T extends ContainerData<any, any, any>> =
+	DepsOfContainer<T> & FlatDependencies<RegisteredDepsOfContainer<T>>;
 
 export type UnionToIntersection<U> = (
 	U extends any ? (k: U) => void : never
