e4fef79169596ef5a7b75cf2e3371f4da51f5354
Lebedev Mikhail <lebedev.mv@tutu.ru>
2023-07-30 01:01:18 +0500

    feat(factory): add type tests for factory from container

--- src/factory.ts
+++ src/factory.ts
@@ -1,4 +1,4 @@
-import { Key } from './containerData';
+import { ContainerData, Dependencies, Key } from './containerData';
 import { Container, createContainer } from './createContainer';
 
 export type FactoryResolve<Params extends Record<Key, any>> = <
@@ -7,10 +7,70 @@ export type FactoryResolve<Params extends Record<Key, any>> = <
 	key: K
 ) => Params[K];
 
-export const factory = <T, Params extends Record<Key, any>>(
-	factory: (getValue: FactoryResolve<Params>) => T
-) =>
-	createContainer({
-		registeredDeps: {},
-		getValue: factory,
-	}) as Container<T, Params, {}>;
+export type Factory = {
+	// factory(container)
+	<
+		T,
+		D extends Dependencies,
+		RD extends Record<Key, ContainerData<any, any, any>>
+	>(
+		container: Container<T, D, RD>
+	): Container<() => T, D, RD>;
+	// factory(container, {param1: 'token1', param2: 'token2'})
+	<
+		T,
+		D extends Dependencies,
+		RD extends Record<Key, ContainerData<any, any, any>>,
+		ParamsMap extends Record<Key, keyof D>
+	>(
+		container: Container<T, D, RD>,
+		params: ParamsMap
+	): Container<
+		(params: FactoryParamsObject<ParamsMap, D>) => T,
+		Omit<D, ParamsMap[keyof ParamsMap]>,
+		RD
+	>;
+	// factory(container, 'token1', 'token2')
+	<
+		T,
+		D extends Dependencies,
+		RD extends Record<Key, ContainerData<any, any, any>>,
+		const Args extends readonly (keyof D)[]
+	>(
+		container: Container<T, D, RD>,
+		...args: Args
+	): Container<
+		(...args: FactoryParamsList<Args, D>) => T,
+		Omit<D, Args[number]>,
+		RD
+	>;
+	// factory((getValue) => getValue('token'))
+	<T, Params extends Record<Key, any>>(
+		factory: (getValue: FactoryResolve<Params>) => T
+	): Container<T, Params, {}>;
+};
+
+export type FactoryParamsObject<
+	ParamsMap extends Record<Key, keyof D>,
+	D extends Dependencies
+> = {
+	[K in keyof ParamsMap]: D[ParamsMap[K]];
+};
+
+export type FactoryParamsList<
+	Args extends readonly (keyof D)[],
+	D extends Dependencies
+> = {
+	[K in keyof Args]: D[Args[K]];
+};
+
+export const factory: Factory = (factory: any) => {
+	if (typeof factory === 'function') {
+		return createContainer({
+			registeredDeps: {},
+			getValue: factory,
+		});
+	}
+
+	return null as any;
+};
