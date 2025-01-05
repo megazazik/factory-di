ba5afc9af23e4fc524edbe420e0407c286ae74e3
Lebedev Mikhail <lebedev.mv@tutu.ru>
2025-01-05 12:43:53 +0300

    in progress

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { UnionToIntersection } from './container';
+import { HumanReadableType, UnionToIntersection } from './container';
 
 export * from './container';
 export * from './factory';
@@ -50,3 +50,126 @@ export * from './awaited';
 
 // const aaa = register({ p1: '345', p2: '12313' });
 // const aaaa = reg({ p1: '345', p2: 12313 });
+
+type Dep<K extends string, V> = /* {
+	[k in K]: V;
+} &  */ {
+	key: K;
+	// value: V;
+	dep: {
+		[k in K]: V;
+	};
+	container: {
+		[k in K]: C<V, any, any>;
+	};
+};
+
+type Ch<K extends string, CC extends C<any, any, any>> = {
+	[k in K]: CC;
+} & {
+	key: K;
+};
+
+type Stub = Ch<never, never>;
+
+export class C<
+	T,
+	D extends Dep<any, any>,
+	RegisteredDeps extends Ch<any, any> | Stub
+> {
+	v: T;
+	__Deps: D;
+	__registeredDeps: RegisteredDeps;
+	__allDeps: AllDeps<this>;
+	// __allDeps: AllDeps<C<D, RegisteredDeps>>
+
+	// register<T extends this[' _allDeps']>(p: T): C<D, RegisteredDeps>;
+	// register(p: HumanReadableType<Omit<AllDeps<this>, 'key'>>): C<D, RegisteredDeps>;
+	// register(p: HumanReadableType<Omit<AllDeps<this>, 'key'>>): C<D, RegisteredDeps>;
+	// register(p: UnionToIntersection<AllDeps<this>>): C<D, RegisteredDeps>;
+	// register(p: this['__allDeps']['dep']): C<D, RegisteredDeps>;
+	// register<T extends this['__allDeps']['dep']>(p: T): C<D, RegisteredDeps>;
+	// register1<T extends this['__allDeps']['dep']>(
+	// 	p: Partial<Pick<T, this['__allDeps']['key']>>
+	// ): C<T, D, RegisteredDeps>;
+	// register<T extends this['__allDeps']['container']>(
+	// 	p: Partial<Pick<T, this['__allDeps']['key']>>
+	// ): C<T, D, RegisteredDeps>;
+	// register<T extends this['__allDeps']['container']>(
+	// 	p: T
+	// ): C<T, D, RegisteredDeps> {
+	// 	return {} as any
+	// }
+	register<
+		N extends Partial<UnionToIntersection<this['__allDeps']['container']>>
+	>(p: N): C<T, D, RegisteredDeps & N> {
+		// >(p: N): C<T, D, RegisteredDeps & Pick<N, this['__allDeps']['key']>> {
+		return {} as any;
+	}
+}
+
+// export type ContainerOf<T extends Dep<any, any>> =
+
+export type Register<
+	T,
+	D extends Dep<any, any>,
+	RegisteredDeps extends Ch<any, any> | Stub,
+	AllDeps extends Dep<any, any>
+> = <T extends D>(p: T) => T;
+
+export type CC = C<boolean, Dep<'p1', number> | Dep<'p2', string>, Stub>;
+
+export type CCC = C<
+	boolean,
+	Dep<'p1', number> | Dep<'p2', string>,
+	Ch<'p1', C<number, Dep<'p3', number> | Dep<'p4', string>, Stub>>
+>;
+
+type AllDeps<T extends C<any, any, any>> =
+	| T['__Deps']
+	| RegsFromAllDeps<T['__registeredDeps']>['__allDeps'];
+type RegsFromAllDeps<R extends Ch<any, any>> = R[R['key']];
+
+type ttt = AllDeps<CC>;
+type tttt = AllDeps<CCC>;
+
+type f = RegsFromAllDeps<CC['__registeredDeps']>;
+type ff = RegsFromAllDeps<CCC['__registeredDeps']>['__allDeps'];
+
+type ggg = HumanReadableType<AllDeps<CCC>>;
+
+export type C4 = C<
+	boolean,
+	Dep<'p5', number> | Dep<'p6', string>,
+	Ch<'p1', CCC>
+>;
+
+type g5 = AllDeps<C4>['dep'];
+type g2 = HumanReadableType<AllDeps<C4>>;
+type g3 = Omit<AllDeps<C4>, 'key'>;
+type g4 = UnionToIntersection<HumanReadableType<AllDeps<C4>['dep']>>;
+
+// ----------------------- register
+declare const c4: C4;
+
+// c4.register({ p1: '123' });
+
+function newC<T>(): C<T, never, Stub> {
+	return null as any;
+}
+
+// c4.register({ p1: newC<number>(), p2: '123' });
+// c4.register1({ p1: 123, p2: '123' });
+const c5 = c4.register({
+	p1: newC<number>(),
+	p2: newC<string>(),
+	p123123: true,
+});
+
+// -----------------------
+
+type AA = (Dep<'p1', number> | Dep<'p2', string>) & Record<string, never>;
+type AAA = AA['key'];
+type AAAA = AA['p1'];
+type AAAAA = AA['p2'];
+type AAAAAA = AA[AA['key']];
